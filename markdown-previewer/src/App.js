0e4dc9fd4a6494b85564e780689a305ad3d32696
nela <nellencr@gmail.com>
2020-03-27 02:41:40 +0200

    css style used to improve the app

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import Form from 'react-bootstrap/Form';
-
 import './App.css';
 let marked = require('marked');
 
@@ -18,19 +16,20 @@ class App extends Component {
   console.log(markdown);
     return (
       <div className="container">
-     <div className="well">
-       <h2 className="text-center">Editor</h2>
-       <textarea className="bg-light" id = "editor" value={markdown} onChange = {(event) => this.updateMarkdown(event.target.value) }>  </textarea>
-     </div>
-    <div className = "bg-light"id = "preview" >
-      <h1></h1>
-      <div dangerouslySetInnerHTML = {{__html: marked(markdown)}} >
-
-      </div>
-    </div> 
-
-        
-      </div>
+         <div className="editor-wrap">
+             <div className="toolbar bg-primary">
+                <i title="no-stack-dub-sack" className="fa fa-free-code-camp"></i> Editor <i className="fa fa-arrows-alt"></i>
+              </div>
+                <textarea className="bg-info" id = "editor" value={markdown} onChange = {(event) => this.updateMarkdown(event.target.value) }>  </textarea>
+          </div>
+          <div className="toolbar headd">
+              <i title="no-stack-dub-sack" className="fa fa-free-code-camp"></i> Editor <i className="fa fa-arrows-alt"></i>
+          </div>
+           <div className = "bg-info" id = "preview" >
+             <div dangerouslySetInnerHTML = {{__html: marked(markdown)}} >
+             </div>
+          </div> 
+       </div>
     )
   }
 }
